280709ceb29166ccac1552001e768438a1010585
castrigno22 <castrigno.nicolo@iisgalvanimi.edu.it>
2022-09-30 09:56:19 +0000

    punto 1.1 non finito rivisitato

--- spotify/src/app/spotify.service.ts
+++ spotify/src/app/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment'
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
@@ -12,14 +13,20 @@ export class SpotifyService {
 
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBOXdlspr-aCPOKpqDErfXXHNwpHhVxThiSNRZemquRZXbvIO6PUjWW6d-f0eQ9V3BjR7RrwRCd6hM7WWK39XQje2Ny0cx0uLDY-AbWvuEe5-0NRJp9qijdS1oTwfFfrNKQZ8ND4YyIgf2Jt-0ACMltvV5noQF5qdcRmdiVdpJVe0fOjnlRA0Pp9czp_EDwOOs'
+    const headers = new HttpHeaders({Authorization:'Bearer BQBOXdlspr-aCPOKpqDErfXXHNwpHhVxThiSNRZemquRZXbvIO6PUjWW6d-f0eQ9V3BjR7RrwRCd6hM7WWK39XQje2Ny0cx0uLDY-AbWvuEe5-0NRJp9qijdS1oTwfFfrNKQZ8ND4YyIgf2Jt-0ACMltvV5noQF5qdcRmdiVdpJVe0fOjnlRA0Pp9czp_EDwOOs'
+      
     });
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
  //Ritorno un observable ai componenti che richiedono il servizio
- 
+  }
+  getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQBOXdlspr-aCPOKpqDErfXXHNwpHhVxThiSNRZemquRZXbvIO6PUjWW6d-f0eQ9V3BjR7RrwRCd6hM7WWK39XQje2Ny0cx0uLDY-AbWvuEe5-0NRJp9qijdS1oTwfFfrNKQZ8ND4YyIgf2Jt-0ACMltvV5noQF5qdcRmdiVdpJVe0fOjnlRA0Pp9czp_EDwOOs'
+    });
+    
+    return this.http.get(url, { headers });
   }
 }
